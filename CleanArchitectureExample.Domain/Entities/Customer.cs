e5e5ed00071bb01f29b8aed250b3285f31df30af
rocco6834740 <roccodeluca865@gmail.com>
2024-11-28 09:51:54 +0100

    added Entities and Value Objects at CleanArchitectureExample.Domain

---
+++ CleanArchitectureExample.Domain/Entities/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureExample.Domain.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public EmailAddress Email { get; private set; }
+        public Customer(Guid id, string name, EmailAddress email) 
+        { 
+            Id = id; 
+            Name = name; 
+            Email = email; 
+        }
+    }
+}
---
+++ CleanArchitectureExample.Domain/ValueObjects/EmailAddress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureExample.Domain.ValueObjects
+{
+    public class EmailAddress
+    {
+        public string? Value { get; private set; }
+        private EmailAddress() { }
+        public EmailAddress(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) 
+            { 
+                throw new ArgumentException("Email address cannot be empty", nameof(value)); 
+            } 
+            // Aggiungi ulteriori validazioni se necessario
+            Value = value; 
+        }
+    }
+}
